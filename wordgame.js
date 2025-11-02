7c888d83ec7145bfcc323889927d0b38611ad041
lsorling <lsorling@yahoo.com.sg>
2025-11-02 03:26:28 +0000

    Nov 2, 2025, 3:26 AM

--- wordgame.js
+++ wordgame.js
@@ -4,12 +4,13 @@ const WORDS = ["cover", "power", "house", "peach", "apple", "bound"];
 function setup() {
     createCanvas(600,600);
     background("hotpink");
+    textAlign(CENTER, CENTER);
+    textSize(40);
+    let selected = random(WORDS);
+    text(selected, width/2, height/2);
+
 }
 
 function draw() {
     background("hotpink");
-    textAlign(CENTER, CENTER);
-    textSize(40);
-    let selected = random(WORDS);
-    text(selected);
 }
