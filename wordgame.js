165fb0fea079991ae916a728cb8e4c85cdc44cb8
lsorling <lsorling@yahoo.com.sg>
2025-11-02 04:46:53 +0000

    Nov 2, 2025, 4:46 AM

--- wordgame.js
+++ wordgame.js
@@ -35,7 +35,7 @@ function handleUserInput() {
     }
     console.log("repeat how many: " + source.length);
     for (let index=0; index<source.length; index++) {
-        
+        console.log(source[index]);
         if (source[index] === target[index]) {
             reveal[index*2] = target[index];
         }
